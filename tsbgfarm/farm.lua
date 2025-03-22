577e4001477a70e37bf2d32a5d643a4ed435b9af
Matiftzy <99849369+Matiftzy@users.noreply.github.com>
2025-03-22 12:17:19 +0800

    Update farm.lua

--- tsbgfarm/farm.lua
+++ tsbgfarm/farm.lua
@@ -1,3 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Matiftzy/Maniez-Hub/refs/heads/main/tsbgfarm/memorysaver.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Matiftzy/Maniez-Hub/refs/heads/main/tsbgfarm/alt.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Matiftzy/Maniez-Hub/refs/heads/main/tsbgfarm/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Matiftzy/Maniez-Hub/refs/heads/main/tsbgfarm/webhook.lua"))()
